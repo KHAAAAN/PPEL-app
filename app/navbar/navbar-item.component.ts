d9068263d4b8aab8004d6f791d488bba51ec029c
Josh Kloster <joshua.kloster@wsu.edu>
2016-04-13 22:58:10 -0700

    Added first iteration of navbar-item functions

--- app/navbar/navbar-item.component.ts
+++ app/navbar/navbar-item.component.ts
@@ -3,6 +3,7 @@ import {Component} from 'angular2/core';
 @Component({
 	selector: 'navbar-item',
 	templateUrl: 'app/navbar/navbar-item.component.html',
+	directives: [Navbar-Item],
 	styleUrls ['app/navbar/navbar.component.css']
 })
 
@@ -12,5 +13,37 @@ export class Navbar-Item {
 	public href: string;
 	public isHover: boolean;
 	public expanded: boolean = false;
+
+	constructor(){
+		this.subItems = [];
+	}
+
+	flip(){
+		if(this.subItems !== undefined && this.subItems.length > 0){
+			this.expanded = !this.expanded;
+		}
+	}
+
+       hoverOn(){
+	       if(this.subItems !== undefined && this.subItems.length > 0){
+		       this.isHover = true;
+	       }
+        }
+
+       hoverOff(this){
+	       if(this.subItems !== undefined && this.subItems.length > 0){
+		       this.isHover = false;
+		}
+	}
+
+
+	checkForHref(){
+		if(this.href === undefined){
+			return "javascript:;";
+		}
+		else{
+			return this.href;
+		}
+	}
 }
 
