7f8dd842aa36924cc43776fc0356e7d129a3f6e2
knott7030@gmail.com <knott7030@gmail.com>
2016-11-15 15:35:30 -0800

    added is admin check

--- client/app/tab/tab-content.component.ts
+++ client/app/tab/tab-content.component.ts
@@ -322,7 +322,7 @@ export class TabContent implements OnInit {
 
 	checkIsLoggedIn(){
 
-		this.setUserModel();
+		//this.setUserModel();
 
 		this._videoService.getPublicVideos()
 			.subscribe((res:any)=>{
